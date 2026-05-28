ab5fb4c4e3f1ecddb84d7f5fd35f3bf1fe07ba58
yuwon <yuwon6881@gmail.com>
2026-05-29 02:09:22 +0800

    CORS

--- FinancialAppApi/Program.cs
+++ FinancialAppApi/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
